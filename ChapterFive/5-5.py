636b0231f38f93484977394d3e6d7f7107c186e7
zhaokai <zhaokai@mbp.local>
2019-11-28 16:44:13 +0800

    5-5

---
+++ ChapterFive/5-5.py
@@ -0,0 +1,15 @@
+alien_color = 'green'
+if alien_color == 'green':
+	print('+5 points')
+elif alien_color == 'yellow':
+	print('+10 points')
+elif alien_color == 'red':
+	print('+15 points')
+
+
+if alien_color == 'green':
+	print('+5 points')
+if alien_color == 'yellow':
+	print('+10 points')
+if alien_color == 'red':
+	print('+15 points')
