a8280972146d4604bdc2b1eb7acd8b838fd6ccf3
zhaokai <zhaokai@mbp.local>
2019-11-28 16:14:57 +0800

    Five

---
+++ ChapterFive/5-3.py
@@ -0,0 +1,10 @@
+alien_color = ['green', 'yellow', 'red']
+
+alien_one = 'green'
+if alien_one in alien_color:
+	print('alien color is green, +5 points')
+
+
+alien_two = 'white'
+if alien_two in alien_color:
+	print('+5 points')
---
+++ ChapterFive/5-4.py
@@ -0,0 +1,11 @@
+alien_color = 'green'
+if alien_color == 'green':
+	print('+5 points')
+if alien_color != 'green':
+	print('+10 points')
+
+if alien_color == 'green':
+	print('+5 points')
+else:
+	print('+10 points')
+
